b8a60f25a15fd35f3de033c4fa9f416dc8f32f5c
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 13:06:47 -0400

    Saving histograms.R

--- histograms.R
+++ histograms.R
@@ -6,3 +6,5 @@ murderPlot<- ggplot(mergedData, aes(x=Murder)) + geom_histogram(bins=10) #creati
 urbanPopPlot <- ggplot(mergedData, aes(x=mergedData$UrbanPop)) + geom_histogram(bins=10) #creating histogram for UrbanPop
 assaultPlot <- ggplot(mergedData, aes(x=mergedData$Assault)) + geom_histogram(bins=10) #creating histogram for Assault
 rapeAssault <- ggplot(mergedData, aes(x=mergedData$Rape)) + geom_histogram(bins=10) #creating histogram for Rape
+populationPlot
+murderPlot
