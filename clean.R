1a0fd6fc4316d11ce5fdb6f8abb926aebefd6ef2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 12:44:32 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,2 +1,6 @@
 
-clean_data <- raw_data
+  dfStates<-dfStates[-c(1,53),]
+  summary(dfStates)
+  str(dfStates)
+  dfStates<-dfStates[,-1:-4]
+  colnames(dfStates)<-c("StateName","population","popOver18","percentOver18")
