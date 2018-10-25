a28f7163b6ca2df751b7c37a821265a048928420
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 12:50:43 -0400

    Saving census.R

--- census.R
+++ census.R
@@ -1,1 +1,11 @@
 
+
+dfStates<-read.csv("scprc_est_2017_18_pop_res.csv", stringsAsFactors = FALSE)
+  head(dfStates)
+  tail(dfStates)
+  dfStates<-dfStates[-c(1,53),]
+  summary(dfStates)
+  str(dfStates)
+  dfStates<-dfStates[,-1:-4]
+  colnames(dfStates)<-c("StateName","population","popOver18","percentOver18")
+  dfStates
